7bcfd6713d3bfd64d104c5a44a9b1b1209794af8
tope101 <temitope@kobo360.com>
2023-10-19 11:19:03 +0100

    make contacts responsive

--- app/components/Tile.jsx
+++ app/components/Tile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Tile({isDarkMode, item}) {
   return (
-    <div className={"p-3.5 flex gap-3 items-center"} style={isDarkMode? {background: "rgb(30 41 59 )", border: "1px solid transparent"} : {background:"#e9e9e9", border: "1px solid #777"}}>
+    <div className={"p-3.5 flex gap-3 items-center w-[140px]"} style={isDarkMode? {background: "rgb(30 41 59 )", border: "1px solid transparent"} : {background:"#e9e9e9", border: "1px solid #777"}}>
         <img src={item.icon} className='h-[25px]'/>
         <p>{item.title}</p>
     </div>
